12764f362fa9c04bff5065186bf17e72919cad83
John-Dav9 <tchomguijohn@gmail.com>
2026-02-19 16:10:12 +0100

    création des variables string et conversion en integer

---
+++ Stagiaires/JohnDavid/08-types.php
@@ -0,0 +1,7 @@
+<?php
+
+// Créons une variable de type string et conertons-la en entier
+$string = "123";
+$integer = (int) $string;
+echo "La variable string : $string\n"; // Affiche : La variable string : 123
+echo "La variable integer : $integer\n"; // Affiche : La variable integer :
