431b5377f25708640a1d44094b0c3fa67acfb6a2
John-Dav9 <tchomguijohn@gmail.com>
2026-02-19 15:44:33 +0100

    modification du fichier 5

--- Stagiaires/JohnDavid/05-php-placement.php
+++ Stagiaires/JohnDavid/05-php-placement.php
@@ -1,3 +1,17 @@
 <?php
 
-echo "Date et heure du serveur : " . date("Y-m-d H:i:s") . "\n";
+$date = date('Y-m-d H:i:s');
+
+echo "<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Date et Heure du Serveur</title> 
+</head>
+<body>
+    <h1>Date et Heure du Serveur</h1>
+    <p>La date et l'heure actuelles du serveur sont : <strong>$date</strong></p>
+</body>
+</html>";
+
