6866ed44433dfd43e8ea43ec486a1de354de5cbc
Bruce-Wuthrich <azertytagada8@gmail.com>
2026-02-19 15:53:36 +0100

    exercice 10

---
+++ Stagiaires/BruceW/10-array-assoc.php
@@ -0,0 +1,22 @@
+<?php
+$monTabAssoc = [
+    "nom" => "wuthrich",
+    "prenom" => "bruce",
+    "age" => 28,
+    "ville" => "bruxelles",
+    "dateDeNaissance" => "1998-02-11",
+    "caracteristique" => "egocentrique"
+];
+echo $monTabAssoc["nom"];
+echo "<br>";
+echo $monTabAssoc["prenom"];
+echo "<br>";
+echo $monTabAssoc["age"];
+echo "<br>";
+echo $monTabAssoc["ville"];
+echo "<br>";
+echo $monTabAssoc["dateDeNaissance"];
+echo "<br>";
+echo $monTabAssoc["caracteristique"];
+echo "<br>";
+?>
