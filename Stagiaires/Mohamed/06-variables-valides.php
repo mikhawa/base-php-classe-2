f115f53b6445db0aa57565d70d4ee635fcb3d17b
Najib-Mohamed <mohanin24@gmail.com>
2026-02-19 15:01:07 +0100

    exo 6 et 7

---
+++ Stagiaires/Mohamed/06-variables-valides.php
@@ -0,0 +1,10 @@
+<?php
+$caractere = "bonjour";
+$nombreentier = 42;
+$nombrefloat = 42.5;
+$estMajeur = true;
+$tableau = [1, 2, 3, 4, 5];
+$tableauAssociatif = ["nom" => "Mohamed", "age" => 30, "ville" => "Bruxelles"];
+$rien = null;
+var_dump($caractere,$nombreentier, $nombrefloat, $estMajeur, $tableau, $tableauAssociatif, $rien);
+?>
---
+++ Stagiaires/Mohamed/07-variables-non-valides.php
@@ -0,0 +1,4 @@
+<?php
+$vraiable-nonvalide = "ceci est une variable non valide";
+$1nombre = 42;
+?>
